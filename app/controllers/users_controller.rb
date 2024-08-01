89d7edb4ffa09a29f945e7e0b181c83718e7c179
tatsukawa <tatsukawatakamasa@gmail.com>
2024-08-02 02:29:28 +0900

    tkinou-3

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       UserMailer.registration_confirmation(@user).deliver_later
       redirect_to @user, notice: 'ユーザー登録が完了しました。'
     else
@@ -28,6 +29,10 @@ class UsersController < ApplicationController
 
   def correct_user
     @user = User.find(params[:id])
-    redirect_to current_user unless current_user?(@user)
+    if current_user && current_user != @user
+      redirect_to current_user
+    elsif !current_user
+      redirect_to login_path, notice: 'ログインが必要です。'
+    end
   end
 end
